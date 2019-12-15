5f0039a319ee86aaeee09e684c46ba5b06d035da
WheezyBackports <55865372+WheezyBackports@users.noreply.github.com>
2019-12-15 13:34:54 -0500

    Shell Script rewritten
    Rewrote 75% of the script to be less garbo and added --dev --update arguments properly.

--- cavegame-start.sh
+++ cavegame-start.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-## Downloads Cave Game source if not already available
-if [ ! -f ~/cavegame/ ]; then
+love2dRUN="./love2d.AppImage cavegame/."
 
-	git clone https://github.com/conariumsoft/cavegame.git
+## Downloads Cave Game source if not already available
+if [ ! -d $PWD/cavegame ]; then
+
+	sh git-repo.sh
 
 fi
-
-## Executes game
-if [ -f love2d.AppImage ]; then 
-
-	./love2d.AppImage cavegame/.
 
 ## Downloads love2d if not already available 	
-else
+if [ ! -f love2d.AppImage ]; then 
 
-	wget -O love2d.AppImage https://bitbucket.org/rude/love/downloads/love-11.3-x86_64.AppImage && chmod +x *.AppImage && ./love2d.AppImage cavegame/.
+	wget -O love2d.AppImage https://bitbucket.org/rude/love/downloads/love-11.3-x86_64.AppImage && chmod +x *.AppImage
 
 fi
 
+## If no arguments are specified run game
+if [ "$1" == "$NULL" ]; then
 
+	$love2dRUN
+
+## If argument is specified run game in developer mode
+elif [ "$1" == "--dev" ]; then
+
+	$love2dRUN -dev
+
+## If argument is specified run update slave script
+elif [ "$1" == "--update" ]; then
+
+	sh git-pull.sh
+
+fi
 
